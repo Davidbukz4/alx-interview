0850db9c0c0358ec56d3a84f590a2e13a4a12839
David Egwuatu <davidegwuatu2@gmail.com>
2023-07-02 16:19:56 +0000

    modified the solution to use recursion

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,25 @@ PASCAL TRIANGLE
 
 
 def pascal_triangle(n):
-    ''' Returns a list of integers representing the pascal triangle'''
-    res = []
+    '''
+    Returns a list of integers representing the pascal triangle for n rows
+    '''
     if n <= 0:
-        return res
-    for i in range(n+1):
-        row = []
-        coef = 1
-        for j in range(1, i+1):
-            row.append(coef)
-            coef = (coef * (i - j)) // j
-        if (len(row)):
-            res.append(row)
-    return res
+        return []
+
+    triangle = []
+    for row in range(n):
+        row_list = []
+        for k in range(row + 1):
+            coef = factorial(row) // (factorial(k) * factorial(row - k))
+            row_list.append(coef)
+        triangle.append(row_list)
+    return triangle
+
+
+def factorial(n):
+    ''' Returns the factorial of a number '''
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
