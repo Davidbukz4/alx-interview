bcbbd37f9337d55690dc728812fb2aac0d7f1b60
David Egwuatu <davidegwuatu2@gmail.com>
2023-08-12 11:15:40 +0000

    added solution

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,18 @@
 #!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(url, (error, resp, body) => {
+  if (error) console.log(error);
+  const characters = JSON.parse(body).characters;
+
+  for (let i = 0; i < characters.length; i++) {
+    request(characters[i], (error, resp, body) => {
+      if (error) console.log(error);
+      const name = JSON.parse(body).name;
+      console.log(name);
+    });
+  }
+});
